16c4f906355c8d374b64c06ba906aaea45a02333
Sean McDermott <mcderm.sean@gmail.com>
2017-04-12 11:49:26 -0700

    show snack page works

--- routes/snacks/index.js
+++ routes/snacks/index.js
@@ -5,11 +5,23 @@ const db = require('../../db/knex.js');
 
 /* GET snacks listing. */
 router.get('/', snacksIndex);
+router.get('/:id', snacksShow);
 
 function snacksIndex(req, res, next) {
   db('snacks')
   .then(snacksIndex => {
     res.render('snacks/index', {snacksIndex});
+  })
+};
+
+function snacksShow(req, res, next) {
+  const id = req.params.id;
+  db('snacks').where({ id }).first()
+  .then(snack => {
+    res.render('snacks/show', { snack })
+  }).catch(err => {
+    console.error(err);
+    next(err)
   })
 };
 
