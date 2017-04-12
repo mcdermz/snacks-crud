d122227494ef880f584fab842749d6b26493c2a3
Sean McDermott <mcderm.sean@gmail.com>
2017-04-12 10:49:33 -0700

    snacks index page now populates with data from DB

--- routes/snacks/index.js
+++ routes/snacks/index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var router = express.Router();
-// const db = require('../../db/knex.js');
+const db = require('../../db/knex.js');
 
 
 /* GET snacks listing. */
 router.get('/', snacksIndex);
 
 function snacksIndex(req, res, next) {
-  res.render('snacks/index');
+  db('snacks')
+  .then(snacksIndex => {
+    res.render('snacks/index', {snacksIndex});
+  })
 };
 
 module.exports = router;
